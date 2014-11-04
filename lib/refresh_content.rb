2f447bd80b21df5d37cf6dbc2e85d2f1911e6463
Maurice <mauricezhang2@gmail.com>
2014-11-03 20:29:11 -0500

    no message

--- lib/refresh_content.rb
+++ lib/refresh_content.rb
@@ -40,7 +40,7 @@ module RefreshContent
 		response = {}
 
 		loop do
-			response = HTTParty.get("http://www.reddit.com/r/nfl/.json")
+			response = HTTParty.get("http://www.reddit.com/r/nfl/.json?sort=new")
 			break if response.code === 200
 		end
 
