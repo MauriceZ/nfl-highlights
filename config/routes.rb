5d15dbb4ae96795d804ee3ef092b895e02e59ac9
Maurice <mauricezhang2@gmail.com>
2014-11-09 15:28:22 -0500

    Change root to current week

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'weeks#index'
+  root :to => 'weeks#show', :id => Week.last.id
   resources :weeks
   resources :welcome
 
